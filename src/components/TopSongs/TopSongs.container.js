3440c13b2c902609afb578bd5f08fc0d9a44210c
Rafael <rbmbika@gmail.com>
2020-08-19 22:26:38 -0300

    Add button to download top songs image

--- src/components/TopSongs/TopSongs.container.js
+++ src/components/TopSongs/TopSongs.container.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 
 import queryString from 'query-string';
+import html2canvas from 'html2canvas';
+import { saveAs } from 'file-saver';
 
 import constants from '../../constants';
 import TopSongsView from './TopSongs.view';
@@ -9,8 +11,28 @@ import { spotifyApi } from '../../api';
 const TopSongsContainer = (props) => {
   const [loading, setLoading] = useState(true);
   const [songs, setSongs] = useState([]);
-
   const { timeRange } = props;
+  let elementId, listTitle;
+
+  if (timeRange === constants.SHORT_TERM) {
+    listTitle = 'Last Month';
+    elementId = 'short-term-list-container';
+  } else if (timeRange === constants.MEDIUM_TERM) {
+    listTitle = 'Last 6 Month';
+    elementId = 'medium-term-list-container';
+  }
+
+  const handleDownloadClick = () => {
+    const element = document.querySelector(`#${elementId}`);
+    html2canvas(element, {
+      useCORS: true,
+      y: element.offsetTop,
+    }).then((canvas) => {
+      canvas.toBlob((blob) => {
+        saveAs(blob, 'my-top-songs.png');
+      });
+    });
+  };
 
   useEffect(() => {
     setLoading(true);
@@ -20,17 +42,21 @@ const TopSongsContainer = (props) => {
       limit: constants.SONGS_LIMIT,
     });
 
-    spotifyApi
-      .get(`/me/top/tracks?${params}`)
-      .then((resp) => {
-        setSongs(resp.data.items);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+    spotifyApi.get(`/me/top/tracks?${params}`).then((resp) => {
+      setSongs(resp.data.items);
+      setLoading(false);
+    });
   }, [timeRange]);
 
-  return <TopSongsView timeRange={timeRange} songs={songs} loading={loading} />;
+  return (
+    <TopSongsView
+      listTitle={listTitle}
+      elementId={elementId}
+      songs={songs}
+      loading={loading}
+      onDownloadClick={handleDownloadClick}
+    />
+  );
 };
 
 export default TopSongsContainer;
--- src/components/TopSongs/TopSongs.view.js
+++ src/components/TopSongs/TopSongs.view.js
@@ -4,36 +4,37 @@ import PropTypes from 'prop-types';
 import { FaSpotify } from 'react-icons/fa';
 
 import SongCard from '../SongInfo';
-import constants from '../../constants';
+import { Button } from 'react-bootstrap';
 import './TopSongs.scss';
-// import { Button } from 'react-bootstrap';
 
 const propTypes = {
   loading: PropTypes.bool,
+  elementId: PropTypes.string,
+  listTitle: PropTypes.string,
+  onDownloadClick: PropTypes.func,
 };
 
 const TopSongsView = (props) => {
-  const { songs, loading, timeRange } = props;
-  const { SHORT_TERM } = constants;
-  const title = timeRange === SHORT_TERM ? 'Last month' : 'Last 6 months';
+  const { songs, loading, elementId, listTitle, onDownloadClick } = props;
 
   return !loading ? (
     <div className="top-songs-component">
-      <div className="title">
-        <FaSpotify className="mr-1" /> My Top Songs
+      <div className="sharableContent" id={elementId}>
+        <div className="title">
+          <FaSpotify className="mr-1" /> My Top Songs
+        </div>
+        <div className="sub-title">{listTitle}</div>
+
+        {songs.map((song, index) => {
+          return <SongCard key={song.id} rank={index + 1} song={song} />;
+        })}
+
+        <div className="site-address">www.mytopsongs.site</div>
       </div>
-      <div className="sub-title">{title}</div>
-
-      {songs.map((song, index) => {
-        return <SongCard key={song.id} rank={index + 1} song={song} />;
-      })}
-
-      <div className="site-address">www.mytopsongs.site</div>
-
-      {/* <div className="download-btn-container">
-        <Button>Download</Button>
-        <span>And Share</span>
-      </div> */}
+      <div className="download-btn-container">
+        <Button onClick={onDownloadClick}>Download</Button>
+        <span className="text">And Share</span>
+      </div>
     </div>
   ) : null;
 };
